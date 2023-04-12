9d4f0a64a90461b4b3be19adfc06b39aeb33b0ea
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-04-12 13:13:29 +0300

    feat: add a solution of count-cats.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
+  const firstLetters = [];
+  members.forEach(el => {
+    if (typeof el === 'string') {
+      firstLetters.push(el.trim()[0].toUpperCase())
+    }
+  })
+  const firstLettersSorted = firstLetters.sort((a, b) => a.localeCompare(b));
+  const dreamTeam = firstLettersSorted.join('');
+  return dreamTeam
 }
 
 module.exports = {

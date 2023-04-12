c634f78a070eae1f7cc5a922e2aadc3a8abb1fd3
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-04-12 19:32:56 +0300

    feat: add a solution of transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  try {
+    // getRectArea(3, 'A');
+  } catch (e) {
+    return e
+  }
+
+  const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]){
+        case '--double-next' :
+          if(i !== arr.length - 1)
+            result.push(arr[i + 1]);
+          break;
+
+        case '--discard-next' :
+          if(i !== arr.length - 1)
+            i++;
+          break;
+
+        case '--double-prev' :
+          if(i !== 0 && arr[i - 2] !== '--discard-next')
+            result.push( result[i-1]);
+          break;
+
+        case '--discard-prev' :
+          if(i !== 0 && arr[i - 2] !== '--discard-next')
+            result.pop();
+          break;
+
+        default :
+          result.push(arr[i]);
+          break;
+
+      }
+  }
+
+  return result
 }
 
 module.exports = {

2c76df66998b8c8c9ca39b341514f37c1fa09fd4
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-04-12 17:10:54 +0300

    fix: a solution of what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -13,24 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   if (date === undefined)  return 'Unable to determine the time of year!';
-  const month = date.getMonth();
-  if (!(date instanceof Date) || date.hasOwnProperty('getMonth')) {
-    throw new Error('"Invalid date!"')
-  }'
-  try {
-    if (month === 0 || month === 1 || month === 11) return 'winter';
-    if (month === 2 || month === 3 || month === 4) return 'spring';
-    if (month === 5 || month === 6 || month === 7) return 'summer';
-    if (month === 8 || month === 9 || month === 10) return 'autumn';
-  } catch (e) {
-    return (e)
-  }
 
+
+  if (!(date instanceof Date) || date.hasOwnProperty('toString')) {
+    throw new Error('Invalid date!')
+  }
+  try {
+
+  } catch (e) {
+
+    return e
+  }
+  const month = date.getMonth();
+  if (month === 0 || month === 1 || month === 11) return 'winter';
+  if (month === 2 || month === 3 || month === 4) return 'spring';
+  if (month === 5 || month === 6 || month === 7) return 'summer';
+  if (month === 8 || month === 9 || month === 10) return 'autumn';
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-
+  // return throw new Error('Invalid date!')
 }
 
 module.exports = {
   getSeason
 };
+// console.log(getSeason(23));

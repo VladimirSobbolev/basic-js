95004e33b14739217750ab8096fe0a87455c7efb
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-04-13 09:45:57 +0300

    feat: add a solution of simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+    // массив легче обработать чем строку
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
+    // длина массива - длина цепи
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.chain.push(`( ${value} )`)
+    return this
+    // добавляем значение в массив
+
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!this.chain[position - 1]  || typeof position !== 'number') {
+      //( проверяю что такой эл. есть в массиве || проверяю что это число, а не строка или типо того)
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!")
+    } else {
+      // подставляю куда надо
+      this.chain.splice(position - 1, 1);
+      return this
+    }
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //получаю строку
+    let chainResult = this.chain.join('~~');
+    //обнавляю цепь
+    this.chain = [];
+    return chainResult;
   }
 };
 
